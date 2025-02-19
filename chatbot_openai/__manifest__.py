6a358d95dda119e88430e1341a8acc5dadb0caba
EdinsonD-Sellside <edinson.diaz@sellside.cl>
2025-02-19 19:12:27 -0300

    estructura de prueba

---
+++ chatbot_openai/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Chatbot OpenAI',
+    'version': '1.0',
+    'description': 'Módulo para integrar OpenAI con Odoo',
+    'depends': ['base'],
+    'data': [
+        'views/views.xml',
+    ],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
