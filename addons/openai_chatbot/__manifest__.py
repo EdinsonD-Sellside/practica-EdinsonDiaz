dc78eef75e83208d00e42d0d1715ac42eca26dab
EdinsonD-Sellside <edinson.diaz@sellside.cl>
2025-02-19 15:15:34 -0300

    estructura_modulo_openai_chatbot

---
+++ addons/openai_chatbot/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Mi Modulo Chatbot',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Modulo para integrar OpenAI con Odoo',
+    'description': """
+        Este módulo integra la API de OpenAI para generar respuestas de un chatbot.
+    """,
+    'author': 'EdinsonD',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
