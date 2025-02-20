5862ddc3322e510b4f42b4e72d73be9388452be8
EdinsonD-Sellside <edinson.diaz@sellside.cl>
2025-02-20 00:21:29 -0300

    Delete openai_chatbot directory

--- openai_chatbot/models/models.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class ChatbotConfig(models.Model):
-    _name = 'chatbot.config'
-    _description = 'Configuración del Chatbot'
-
-    api_key = fields.Char(string="API Key de OpenAI")
