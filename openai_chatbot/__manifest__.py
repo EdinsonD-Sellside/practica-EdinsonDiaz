c2c20490e3e561adfd16b3f3e4cba28d1de563bf
EdinsonD-Sellside <edinson.diaz@sellside.cl>
2025-02-20 02:56:33 -0300

    cambios

---
+++ openai_chatbot/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'OpenAI Chatbot',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Integration with OpenAI Chatbot',
+    'description': """Integration with OpenAI Chatbot to send and receive messages.""",
+    'author': 'Your Name',
+    'website': 'http://www.yourcompany.com',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/security.xml',
+        'views/chatbot_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
