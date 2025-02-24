a3f156477a7320e23fb4cbdedd08b1bf5e8f6d1c
EdinsonD-Sellside <edinson.diaz@sellside.cl>
2025-02-24 02:57:51 -0300

    Actualizacion __manifest__.py

--- custom_addons/chatgpt_openai/__manifest__.py
+++ custom_addons/chatgpt_openai/__manifest__.py
@@ -1,11 +1,15 @@
 {
-    'name': 'AI Chat',
+    'name': 'IA_openai_chatbot_2025_1-link',
     'version': '1.0',
-    'author': 'Diego',
+    'author': 'Edinson',
     'depends': ['base', 'mail'],
     'data': [
         'data/ai_bot_data.xml',
     ],
     'installable': True,
     'application': True,
+    'auto_install': False,  # No se instala automáticamente
+    'external_dependencies': {
+        'python': ['openai'],
+    },
 }
