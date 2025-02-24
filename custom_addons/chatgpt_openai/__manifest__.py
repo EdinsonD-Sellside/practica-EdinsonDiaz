f4f23408ff3b1b90aa6eb4d7bbf0e493b88b0880
EdinsonD-Sellside <edinson.diaz@sellside.cl>
2025-02-24 02:52:16 -0300

    estructura_plagio_Diego

---
+++ custom_addons/chatgpt_openai/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'AI Chat',
+    'version': '1.0',
+    'author': 'Diego',
+    'depends': ['base', 'mail'],
+    'data': [
+        'data/ai_bot_data.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
